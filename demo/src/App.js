1b78f45c3edca270766fa23ed8c68925bbc1addf
Robert Pym <rob_pym@hotmail.com>
2020-05-05 13:52:28 +0100

    Functional style to Component class

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Learn React TDD</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Learn React TDD</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
